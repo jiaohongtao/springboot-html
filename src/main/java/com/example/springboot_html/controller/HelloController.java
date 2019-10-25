74353125f84c356ca46bcc16051019878fb483e4
jiaoht <18332120276@163.com>
2019-10-25 17:30:14 +0800

    第8次提交

--- src/main/java/com/example/springboot_html/controller/HelloController.java
+++ src/main/java/com/example/springboot_html/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.springboot_html.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
---
+++ src/main/java/com/example/springboot_html/test/TestLambda.java
@@ -0,0 +1,18 @@
+package com.example.springboot_html.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author jiaohongtao
+ * @since 2019/10/25 17:21
+ */
+public class TestLambda {
+
+	public static void main(String[] args) {
+		List<String> proNames = Arrays.asList("Ni", "Hao", "Lambda");
+		List<String> uppercaseNames = proNames.stream().map(String::toUpperCase).collect(Collectors.toList());
+		System.out.println(uppercaseNames);
+	}
+}
