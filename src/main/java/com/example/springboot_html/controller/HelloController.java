ee554d683bf2be7bdd2c41e8b1b83f9ff7107edd
jiaohongtao <jiaohongtao@beyondcent.com>
2020-04-15 15:42:21 +0800

    继承swagger，生成在线文档

--- src/main/java/com/example/springboot_html/controller/HelloController.java
+++ src/main/java/com/example/springboot_html/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.springboot_html.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2019/8/22 16:39
  */
 @RestController
+@Api
 public class HelloController {
 
 	@RequestMapping({"", "/index"})
+	@ApiOperation(value = "首页(hello)", httpMethod = "GET")
 	public String index() {
 		return "hello";
 	}
