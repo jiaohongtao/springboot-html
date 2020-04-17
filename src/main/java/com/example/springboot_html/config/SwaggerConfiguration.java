d0571237c0dd35d8c6e11dd5437b3c3623792774
jiaohongtao <jiaohongtao@beyondcent.com>
2020-04-17 11:19:22 +0800

    规范配置路径

--- src/main/java/com/example/springboot_html/config/SwaggerConfiguration.java
+++ src/main/java/com/example/springboot_html/config/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.springboot_html;
+package com.example.springboot_html.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
