3b854230f1e8c2735a53266cacdbc68bb933c4f2
jiaohongtao <jiaohongtao@beyondcent.com>
2020-06-29 09:51:15 +0800

    springboot使用EnableScheduling定时注解

--- src/main/java/com/example/springboot_html/SpringbootHtmlApplication.java
+++ src/main/java/com/example/springboot_html/SpringbootHtmlApplication.java
@@ -2,8 +2,10 @@ package com.example.springboot_html;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringbootHtmlApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/springboot_html/util/date20200612/TestSchedule.java
@@ -0,0 +1,28 @@
+package com.example.springboot_html.util.date20200612;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+
+/**
+ * 测试定时器    涉及注解@EnableScheduling @Scheduled @Component
+ * <p>
+ * href: https://www.cnblogs.com/chenpi/p/9785953.html
+ *
+ * @author jiaohongtao
+ * @version 1.0
+ * @since 2020年06月29日
+ */
+@Component
+public class TestSchedule {
+
+    private final Logger logger = LoggerFactory.getLogger(TestSchedule.class);
+
+    @Scheduled(initialDelay = 1000, fixedRate = 10000)
+    public void run() {
+        logger.info("Current time is :: " + Calendar.getInstance().getTime());
+    }
+}
