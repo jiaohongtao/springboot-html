4a7fa074476ab6d481bc3830ef9443cca2af619f
jiaohongtao <jiaohongtao@beyondcent.com>
2020-07-24 15:03:54 +0800

    测试两个Service实现一个接口，使用@Autowired,@Qualifier与@Resource调用

---
+++ src/main/java/com/example/springboot_html/interf/AService.java
@@ -0,0 +1,12 @@
+package com.example.springboot_html.interf;
+
+import org.python.antlr.ast.Str;
+
+/**
+ * @author jiaohongtao
+ * @version 1.0
+ * @since 2020年07月24日
+ */
+public interface AService {
+    String a();
+}
