23f6490f4dad5ca9f3bdd5fa92ab33f1eb978a0e
jiaohongtao <jiaohongtao@beyondcent.com>
2020-09-09 15:42:27 +0800

    屏蔽Mysql

--- src/main/java/com/example/springboothtml/SpringbootHtmlApplication.java
+++ src/main/java/com/example/springboothtml/SpringbootHtmlApplication.java
@@ -3,11 +3,9 @@ package com.example.springboothtml;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJdbcHttpSession
 public class SpringbootHtmlApplication {
 
 	public static void main(String[] args) {
