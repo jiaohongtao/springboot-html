3396d5a7760fb988e569c2257c7b99751fbdd045
jiaohongtao <jiaohongtao@beyondcent.com>
2020-09-09 13:47:25 +0800

    添加说明功能页面

---
+++ src/main/java/com/example/springboothtml/controller/FunctionDescriptionController.java
@@ -0,0 +1,26 @@
+package com.example.springboothtml.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 功能说明Controller
+ *
+ * @author jiaohongtao
+ * @version 1.0
+ * @since 2020年09月09日
+ */
+@Api("功能说明")
+@Controller
+public class FunctionDescriptionController {
+
+    @ApiOperation("跳转页面")
+    @GetMapping("/function/description")
+    public String functionDescription() {
+        return "functionDescription";
+    }
+}
+
