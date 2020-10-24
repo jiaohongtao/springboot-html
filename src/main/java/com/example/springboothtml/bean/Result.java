e2e9fd79fb76c61851d3560a41a2746794951fe8
jiaoht <18332120276@163.com>
2020-10-24 16:25:50 +0800

    统一返回值

---
+++ src/main/java/com/example/springboothtml/bean/Result.java
@@ -0,0 +1,36 @@
+package com.example.springboothtml.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 返回结果
+ *
+ * @author jiaohongtao
+ * @version 1.0
+ * @since 2020/10/24 16:10
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result {
+    private boolean success;
+    private Object data;
+    private String message;
+    private int code;
+
+    public Result(boolean success, Object data, String message) {
+        this.success = success;
+        this.data = data;
+        this.message = message;
+    }
+
+    public static Result SUCCESS(Object data, String message) {
+        return new Result(true, data, message);
+    }
+
+    public static Result FAILED(Object data, String message) {
+        return new Result(false, data, message);
+    }
+}
