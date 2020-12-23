400da1e6c2e071fb548a5af138b720241753cd5d
jiaohongtao <jiaohongtao@beyondcent.com>
2020-12-23 17:54:33 +0800

    提供性别和年龄获取

--- src/main/java/com/hong/controller/ProvinceController.java
+++ src/main/java/com/hong/controller/ProvinceController.java
@@ -12,13 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @version 1.0
  * @since 2020年12月23日
  */
-@Api(tags = "获取行政地址")
+@Api(tags = "获取身份证信息")
 @RestController
 public class ProvinceController {
-
-    @ApiOperation(value = "省级", httpMethod = "GET")
+    @ApiOperation(value = "省级地区", httpMethod = "GET")
     @GetMapping("/province/{idNumber}")
     public String getProvince(@PathVariable String idNumber) {
         return IDCardUtil.getProvince(idNumber);
+    }
+
+    @ApiOperation(value = "性别", httpMethod = "GET")
+    @GetMapping("/sex/{idNumber}")
+    public String getSex(@PathVariable String idNumber) {
+        return IDCardUtil.judgeGender(idNumber);
+    }
+
+    @ApiOperation(value = "年龄", httpMethod = "GET")
+    @GetMapping("/age/{idNumber}")
+    public int getAge(@PathVariable String idNumber) {
+        return IDCardUtil.countAge(idNumber);
     }
 }
